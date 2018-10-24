d152019c3345b1b2e783defc338bd4e81170cfd9
leavalos <l.e.avalos1@gmail.com>
2018-10-24 02:13:03 -0300

    Bomberman ya puede recoger poder para lanzar multiples bombas y puede utilizar este. Se implementa clase ProtoMaxUnits.

---
+++ src/main/java/gradle/cucumber/boss/ProtoMaxUnits.java
@@ -0,0 +1,10 @@
+package gradle.cucumber.boss;
+
+import gradle.cucumber.superpower.SuperPowerThrowMultipleBombs;
+
+
+public class ProtoMaxUnits extends Boss {
+    public ProtoMaxUnits() {
+        super(new SuperPowerThrowMultipleBombs());
+    }
+}
